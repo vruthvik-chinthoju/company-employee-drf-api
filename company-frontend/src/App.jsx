b7f85a2bf3abbee2215ba837c3c837ce725c3ca1
ennter usernane <vruthvik.chinthoju@gmail.com>
2026-03-12 14:10:51 +0530

    burger navigation

--- company-frontend/src/App.jsx
+++ company-frontend/src/App.jsx
@@ -7,6 +7,8 @@ import CreateCompany from "./components/CreateCompany";
 import CompanyList from "./components/CompanyList";
 import Bar from "./components/NavBar";
 
+import "./App.css"
+
 import { getEmployees } from "./services/api";
 
 function App() {
@@ -30,11 +32,26 @@ function App() {
       <Routes>
 
         <Route
+          path="/"
+          element={
+            <div style={{ textAlign: "center" }}>
+              <div className="box">
+                <div className="home">
+                  <h1>HR Management System</h1>
+                  <img src="https://img.freepik.com/premium-vector/people-doodle-businessman-sitting-front-his-computer-hand-drawn-cartoon-illustration_3890-639.jpg?w=360" alt="" />
+                  <p>Manage companies and employees easily</p>
+                </div>
+              </div>
+            </div>
+          }
+        />
+
+        <Route
           path="/employees"
           element={
             <>
               <CreateEmployee loadEmployees={loadEmployees} />
-              <EmployeeList employees={employees} loadEmployees={loadEmployees}/>
+              <EmployeeList employees={employees} loadEmployees={loadEmployees} />
             </>
           }
         />
--- company-frontend/src/components/NavBar.jsx
+++ company-frontend/src/components/NavBar.jsx
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { useState } from "react";
 
 function Bar() {
+
+  const [open ,setOpen] = useState(false)
+
+  const handleClick = () =>{
+    setOpen(!open)
+  }
+
   return (
     <div className="navbar">
       <div className="main">
         <img src="https://www.creativefabrica.com/wp-content/uploads/2019/02/Monogram-HR-Logo-Design-by-Greenlines-Studios-580x387.jpg" alt="logo" />
         <h3 className="title">HR-Management System</h3>
       </div>
-      <div className="links">
+      <div className={`links ${open ? "active" : ""}`}>
         <Link className="link" to="/employees">Employees</Link>
         <Link className="link" to="/companies">Companies</Link>
+      </div>
+      <div className="close">
+        <button onClick={handleClick}> ☰</button>
       </div>
     </div>
   );
